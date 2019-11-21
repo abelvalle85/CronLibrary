bd14220ce1ba2e0e31c509d89508de774b9ea971
abelvalle85 <abel.valle@scalablepress.com>
2019-11-21 17:26:47 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -2,6 +2,7 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     //echo "Hello, ${name}."
+    fillCron()
 
     def schedule = fillCron()
 
@@ -14,9 +15,7 @@ def call(String name = 'human') {
 
         //fillCron()
         triggers {
-            parameterizedCron('''
-            echo $schedule
-        ''')
+            parameterizedCron(''' echo $schedule ''')
         }
 
         stages {
--- vars/fillCron.groovy
+++ vars/fillCron.groovy
@@ -1,9 +1,11 @@
 def call (){
-    '''
+    def sc ='''
        H/2 * * * * %RUN_ENV=production
        H/3 * * * * %SERVICE=case
        H/3 * * * * %SERVICE=inventory
        H/3 * * * * %SERVICE=router
        H/3 * * * * %SERVICE=shipping
         '''
+
+    return sc
 }
