05268b69e9176741bb4bdda150602e6fbd6e0786
abelvalle85 <abel.valle@scalablepress.com>
2019-11-21 16:47:58 -0600

    Adding crons

--- vars/fillCron.groovy
+++ vars/fillCron.groovy
@@ -1,7 +1,9 @@
 def call (){
-    echo """H/2 * * * * %RUN_ENV=production
+    parameterizedCron('''
+       H/2 * * * * %RUN_ENV=production
        H/3 * * * * %SERVICE=case
        H/3 * * * * %SERVICE=inventory
        H/3 * * * * %SERVICE=router
-       H/3 * * * * %SERVICE=shipping"""
+       H/3 * * * * %SERVICE=shipping
+        ''')
 }
