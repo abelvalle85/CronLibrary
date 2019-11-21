34426e87a80544cade88acb8ccb645fbd6aeac16
abelvalle85 <abel.valle@scalablepress.com>
2019-11-21 17:15:34 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,8 +1,29 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Hello, ${name}."
+    //echo "Hello, ${name}."
 
-    fillCron()
+    //fillCron()
+
+    pipeline {
+        agent any
+        parameters {
+            string(name: 'RUN_ENV', defaultValue: 'stage', description: 'Which environment will run?')
+            string(name: 'SERVICE', defaultValue: 'dashboard', description: 'Service to run')
+        }
+        triggers {
+            parameterizedCron('''
+        fillCron()
+        ''')
+        }
+        stages {
+            stage("Cron") {
+                steps {
+                    echo "${params.SERVICE} ${params.RUN_ENV}"
+                    script { currentBuild.description = "${params.SERVICE} ${params.RUN_ENV}" }
+                }
+            }
+        }
+    }
 
 }
