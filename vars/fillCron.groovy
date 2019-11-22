1340daf7317e58ee49d43d7379319893866d780d
abelvalle85 <abel.valle@scalablepress.com>
2019-11-22 16:16:29 -0600

    Adding crons

--- vars/fillCron.groovy
+++ vars/fillCron.groovy
@@ -1,7 +1,10 @@
-def call (Map pipelineParams){
-    sh  'echo """H/2 * * * * %RUN_ENV=production
+def call (Map pipelineParams) {
+    def cron = """H/2 * * * * %RUN_ENV=production
        H/3 * * * * %SERVICE=case
        H/3 * * * * %SERVICE=inventory
        H/3 * * * * %SERVICE=router
        H/3 * * * * %SERVICE=shipping
-        """'
+        """
+
+    return cron
+}
