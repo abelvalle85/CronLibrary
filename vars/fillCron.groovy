9c839b904fc731dd1929e981489e0c78c7c4dcd1
abelvalle85 <abel.valle@scalablepress.com>
2019-11-22 16:22:42 -0600

    Adding crons

--- vars/fillCron.groovy
+++ vars/fillCron.groovy
@@ -1,9 +1,8 @@
 def call (Map pipelineParams) {
-    def cron = """H/2 * * * * %RUN_ENV=production
-       H/3 * * * * %SERVICE=case
-       H/3 * * * * %SERVICE=inventory
-       H/3 * * * * %SERVICE=router
-       H/3 * * * * %SERVICE=shipping
-        """
-
+    def cron = ["H/2 * * * * %RUN_ENV=production",
+       "H/3 * * * * %SERVICE=case",
+       "H/3 * * * * %SERVICE=inventory",
+       "H/3 * * * * %SERVICE=router",
+       "H/3 * * * * %SERVICE=shipping "]
+    return cron
 }
