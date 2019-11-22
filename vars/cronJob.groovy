00cc4707556885cb9356012f56452d1c47364a5f
abelvalle85 <abel.valle@scalablepress.com>
2019-11-22 17:26:32 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -25,8 +25,8 @@ def call(Map pipelineParams) {
            // node {
                 stage("Cron") {
                     steps {
-                        echo "${params.SERVICE} ${params.SCRIPT} ${params.RUN_ENV}"
-                        script { currentBuild.description = "${params.SERVICE} ${params.RUN_ENV}" }
+                        echo "${params.SERVICE} ${params.RUN_ENV}"
+                        script { currentBuild.description = "${params.SERVICE} ${params.SCRIPT} ${params.RUN_ENV}" }
                         //sh 'PROJECT=oo SERVICE=case . /var/oo/etc/scripts/init-nvm.sh '
                         echo "PROJECT=oo SERVICE=${params.SERVICE} . /var/oo/etc/scripts/init-nvm.sh "
                         //sh 'NODE_ENV=production coffee /var/oo/oo-case/crons/reject2.coffee"
