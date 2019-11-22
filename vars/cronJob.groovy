befc0119a59bb02caf45ced8ba9a0aa32a8832d9
abelvalle85 <abel.valle@scalablepress.com>
2019-11-22 11:28:06 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -1,25 +1,13 @@
 def call(Map pipelineParams) {
-    node {
+    pipeline {
         agent any
-            triggers {
-                parameterizedCron(fillCron())
-            }
+        triggers {
+            parameterizedCron(fillCron())
+        }
         //parameters {
           //  string(name: 'RUN_ENV', defaultValue: 'stage', description: 'Which environment will run?')
           //  string(name: 'SERVICE', defaultValue: 'dashboard', description: 'Service to run')
-       // }
 
-       // fillCron()
-
-    /* triggers {
-            parameterizedCron('''
-               H/2 * * * * %RUN_ENV=production
-               H/3 * * * * %SERVICE=case
-               H/3 * * * * %SERVICE=inventory
-               H/3 * * * * %SERVICE=router
-               H/3 * * * * %SERVICE=shipping
-                ''')
-        }*/
         stages {
             stage("Cron") {
                 steps {
