ff556613f735e87a53632d165f6b806a51f2f560
abelvalle85 <abel.valle@scalablepress.com>
2019-11-25 15:29:31 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -3,7 +3,7 @@ import jenkins.model.*
 jenkins = Jenkins.instance
 
 def call(Map pipelineParams) {
-//def cron=fillCron()
+def fc= new fillCron()
 
     pipeline {
         agent any
@@ -22,7 +22,7 @@ def call(Map pipelineParams) {
             H/20 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updateOrder/importMissing
             H/35 * * * * % SERVICE=inventory;SCRIPT=updateOrder/importMissing
             H/5 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updatePrimeInventory/pullAos """)*/
-            parameterizedCron(""" ${fillCron()} """)
+            parameterizedCron(""" ${fc.cron} """)
         }
         stages {
            // node {
