08948e8aff678f0193d1077b4ca29ddf1dbfcb03
abelvalle85 <abel.valle@scalablepress.com>
2019-11-25 15:09:22 -0600

    Adding crons

--- vars/cronJob.groovy
+++ vars/cronJob.groovy
@@ -22,7 +22,7 @@ def cron=fillCron()
             H/20 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updateOrder/importMissing
             H/35 * * * * % SERVICE=inventory;SCRIPT=updateOrder/importMissing
             H/5 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updatePrimeInventory/pullAos """)*/
-            parameterizedCron(cron)
+            parameterizedCron(${cron})
         }
         stages {
            // node {
