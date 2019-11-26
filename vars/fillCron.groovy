074e2209fb800e42e22f6484575fa6b2caac4858
abelvalle85 <abel.valle@scalablepress.com>
2019-11-26 10:16:08 -0600

    Adding crons

--- vars/fillCron.groovy
+++ vars/fillCron.groovy
@@ -1,6 +1,6 @@
 def call (Map pipelineParams) {
     //getCron(){
-    def cron="""H/2 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=alpha-broder-test
+    /*def cron="""H/2 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=alpha-broder-test
             H/40 * * * * % SERVICE=inventory;SCRIPT=alpha-broder-test
             H/10 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=orderBlanks/order
             H/50 * * * * % SERVICE=inventory;SCRIPT=orderBlanks/order
@@ -8,8 +8,9 @@ def call (Map pipelineParams) {
             H/55 * * * * % SERVICE=inventory;SCRIPT=rejections/pruneRejections
             H/20 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updateOrder/importMissing
             H/35 * * * * % SERVICE=inventory;SCRIPT=updateOrder/importMissing
-            H/5 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updatePrimeInventory/pullAos"""
-        return cron
+            H/5 * * * * % RUN_ENV=production;SERVICE=inventory;SCRIPT=updatePrimeInventory/pullAos"""*/
+    def cron = readFile.readFileInList("cron.txt")
+    return cron
     //}
     /*def lines = readFile.readFileInList("cron.txt")
     println lines*/
